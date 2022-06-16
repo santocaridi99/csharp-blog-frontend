e5bf11395282430c665aec34ad74b3266ce28444
santocaridi99 <santocaridi99@gmail.com>
2022-06-16 12:38:08 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using csharp_blog_frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace csharp_blog_frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csharp_blog_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
